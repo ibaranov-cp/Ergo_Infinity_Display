49b90451794d4a31d8b813e97d71f05a61fd8901
Ilia Baranov <ibaranov@clearpathrobotics.com>
2015-12-30 16:28:11 -0500

    Working Demo to Show System data
    Shows CPU, MEM, DSK, Net settings on the LCD

--- CPU_GPU.py
+++ CPU_GPU.py
@@ -1,10 +1,72 @@
 #!/usr/bin/env python
 
 # Author: Ilia Baranov
+# Very simple demo of showing system stats on the Keyboard LCD
 
 import wmi
 import os 
+import psutil
+from time import strftime, time
+
+from Ergo_Infinity_Display import *
+lcd = [[0 for x in range(32)] for x in range(128)] 
 
 w = wmi.WMI(namespace='root\\wmi')
-print (w.MSAcpi_ThermalZoneTemperature()[0].CurrentTemperature/10.0)-273.15
 
+
+if __name__ == '__main__':		
+	
+	ser = serial.Serial(28, 115200, timeout=0.5) #Change to (Serial port - 1) on Windows.
+	ser.close()
+	ser.open()
+	
+	
+	# Keeping track of Incoming + outgoing net traffic
+	l_net = psutil.net_io_counters().bytes_recv + psutil.net_io_counters().bytes_sent
+	l_time = time()
+	
+	try:
+		while True:
+			temp = (w.MSAcpi_ThermalZoneTemperature()[0].CurrentTemperature/10.0)-273.15 #Temp of CPU (WINDOWS ONLY, Linux needs to use sensors)
+			cpu = psutil.cpu_percent(interval=None) #CPU Usage
+			mem = psutil.virtual_memory().percent #Memory Usage
+			disk =  psutil.disk_usage('/').percent
+			net = ((psutil.net_io_counters().bytes_recv + psutil.net_io_counters().bytes_sent) - l_net)/(time()-l_time) # Messy, lousy net estimate
+			l_time = time()
+			l_net = psutil.net_io_counters().bytes_recv + psutil.net_io_counters().bytes_sent
+			
+			if (mem >= 60): lcd_color(65535,65535,0,ser) #RGB colors with max value of 65535 (16 bit) each			
+			elif (mem >= 80): lcd_color(65535,0,0,ser)
+			else: lcd_color(65535,65535,65535,ser) #RGB colors with max value of 65535 (16 bit) each
+			
+			for x in range (len(lcd)): #Clean old LCD
+				for y in range (len(lcd[0])):
+					lcd[x][y] = 0
+			
+			#Print date and time
+			send_string(lcd,strftime("%m-%d"),103,24)
+			send_string(lcd,strftime("%H:%M"),103,16)
+			
+			#Show CPU, MEM, etc loading in bar graphs
+			y_t = 24
+			send_string(lcd,"CPU [", 0, y_t)
+			send_string(lcd,"]", 76, y_t)
+			send_string(lcd,"" * int(round(cpu/10)), 26, y_t) # Represent bar graph for CPU usage (total)
+			y_t -=8
+			send_string(lcd,"MEM [", 0, y_t)
+			send_string(lcd,"]", 76, y_t)
+			send_string(lcd,"" * int(round(mem/10)), 26, y_t) # Represent bar graph for Mem
+			y_t -=8
+			send_string(lcd,"DSK [", 0, y_t)
+			send_string(lcd,"]", 76, y_t)
+			send_string(lcd,"" * int(round(disk/10)), 26, y_t) # Represent bar graph for Disk usage
+			y_t -=8
+			send_string(lcd,"NET ", 0, y_t)
+			send_string(lcd,"{0:.2f}".format(net/1048576) + " Mbps", 24, y_t)
+			
+			send(lcd,ser) #Update LCD all at once
+			
+	except KeyboardInterrupt: # Press Ctrl + C to exit
+		print 'Exit'
+	
+	ser.close()
--- Ergo_Infinity_Display.py
+++ Ergo_Infinity_Display.py
@@ -20,26 +20,28 @@ lcd[2][0]=1 # Test pattern to ensure orientation
 lcd[0][2]=1
 
 # Fairly painless way to do fonts		
-def send_char(array,c,x,y): #Enter a char at location x,y using small 8x8 font
-	for w in range (8):	
-		array[x+w][y:y+8] = list(format(Small_Fonts8x8[(ord(c)-32)*8+w], '08b'))
+def send_char(array,c,x,y): #Enter a char at location x,y using small 5x8 font
+	for w in range (5):	
+		#if (x+w <= len(array)) & (y+8 <= len(array[0])): #Boundrary checking
+		array[x+w][y:y+8] = list(format(QuickType_5x8[(ord(c)-32)*5+w], '08b'))
 		
 def send_string(array,str,x,y):
 	for i, c in enumerate (str):
-		send_char(array,c,x+i*8,y)
-
-for x in range (54,74):
-	for y in range (6,26):
-		lcd[x][y] = 1
+		send_char(array,c,x+i*5,y)
 		
-def clear(array): # Clean lcd array and clean screen
+def clear(array,ser): # Clean lcd array and clean screen
 	for x in range (len(array)):
 		for y in range (len(array[0])):
 			array[x][y] = 0
 	ser.write("lcdInit \r")
 	sleep(0.1)
+	
+def lcd_color(r,g,b,ser):
+	command = "lcdColor " + str(r) + " " + str(g) + " " + str(b) + " \r"
+	ser.write(command)
+	sleep(0.05)
 			
-def send(array): #Pass an array, for updating the whole screen, slow!
+def send(array,ser): #Pass an array, for updating the whole screen, slow!
 	for segs in range (8): # have to break into 8 segments of 16 to avoid lcd overload
 		for y in range (len(array[0])/8):
 			command = "lcdDisp " + hex(y) + " " + hex(segs*16) + " "
@@ -53,7 +55,7 @@ def send(array): #Pass an array, for updating the whole screen, slow!
 			#print command
 			sleep(0.03) #Fastest I can go before artefacts start to appear
 
-def update_pixel(array,x,y,val): #Update a single pixel at location x,y, with either 1 or 0
+def update_pixel(array,x,y,val,ser): #Update a single pixel at location x,y, with either 1 or 0
 	array[x][y]= val;
 	ypose = int(floor(y/8))
 	val = ""
@@ -76,15 +78,21 @@ if __name__ == '__main__':
 	ser.close()
 	ser.open()
 	
+	for x in range (54,74):
+		for y in range (6,26):
+			lcd[x][y] = 1
+	
 	send_char(lcd,'!', 4, 0)
 	
-	send_string(lcd,"TEST 1,2,3", 4, 10)
+	send_string(lcd,"TEST 1,2,3 ", 4, 10)
+	send_string(lcd,"", 4, 0)
+
 	
 	#This chunk shows time it takes for different operations
 	print "\n\rTiming Tests"
-	print "Update Full LCD: " + str(timeit("send(lcd)",setup="from __main__ import send,lcd",number=1)) # Send 3 dot test pattern
-	print "Clear LCD: " + str(timeit("clear(lcd)",setup="from __main__ import clear,lcd",number=1)) # Clear LCD array, and clear screen
-	print "Update 1 individual pixel: " + str(timeit("update_pixel(lcd,1,1,1)",setup="from __main__ import update_pixel,lcd",number=1)) # Send individual pixels, while keeping exisiting data from array
+	print "Update Full LCD: " + str(timeit("send(lcd,ser)",setup="from __main__ import send,lcd",number=1)) # Send 3 dot test pattern
+	print "Clear LCD: " + str(timeit("clear(lcd,ser)",setup="from __main__ import clear,lcd",number=1)) # Clear LCD array, and clear screen
+	print "Update 1 individual pixel: " + str(timeit("update_pixel(lcd,1,1,1,ser)",setup="from __main__ import update_pixel,lcd",number=1)) # Send individual pixels, while keeping exisiting data from array
 
 	 # Fill LCD array with block of  checkerboard data
 	v = 1
